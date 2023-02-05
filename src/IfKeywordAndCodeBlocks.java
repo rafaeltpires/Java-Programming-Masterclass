f6145a1fa3a3c227aadfece027d55846871b8b71
RAFA-FIXO\jorge <rafael.pires@hcpro.pt>
2023-02-05 23:00:04 +0000

    43. Code Blocks And The If Then Else Control Statement

--- src/IfKeywordAndCodeBlocks.java
+++ src/IfKeywordAndCodeBlocks.java
@@ -1,4 +1,28 @@
 public class IfKeywordAndCodeBlocks
 {
-    
+    public static void main(String[] args)
+    {
+        boolean gameOver = true;
+        int score = 5000;
+        int levelcompleted = 5;
+        int bonus = 1000;
+
+        // if() else
+        if (score < 5000) {
+            System.out.println("Your Score was less than 5000");
+        } else {
+            System.out.println("Got here");
+        }
+        
+        // if() else if() else
+        if ( score < 5000 && score > 1000 ) {
+            System.out.println("Your Score was less than 5000 but greater than 1000");
+        } else if ( score < 1000 ) {
+            System.out.println("Your Score was less than 1000");
+        } else {
+            System.out.println("Got here");
+        }
+
+
+    }
 }
